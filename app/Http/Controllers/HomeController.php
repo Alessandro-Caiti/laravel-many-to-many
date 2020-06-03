07a3582f267c02d48cf4ecdbcb0630a998fe68fe
Alessandro Caiti <acaiti@hotmail.it>
2020-06-03 16:50:13 +0200

    Recupero vecchia repo, iniziando a capire (vedi many-to -many-remake)

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Category;
+use App\InfoUser;
+use App\Page;
+use App\Photo;
+use App\Tag;
+use App\User;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = User::find(1);
+        $page = Page::find(1);
+        // dd($user->info->bio);
+        // dd($user->categories);
+        // dd($user->photos);
+        // dd($user->tags);
+        // dd($user->pages);
+        // dd($page->tags);
+        return view('home');
+    }
+}
